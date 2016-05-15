691f4539ebe8167c8a7a0e6ea459ce7fd478b9d2
SimoRihani <smimo22@gmail.com>
2016-05-16 00:03:09 +0200

    up

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p 'Heuristic to use (1: h1, 2: h2) : ' h
+read -p 'Instance path : ' path
+
+cd src/
+
+if [[ $h = 1 ]]; then
+  h="h1"
+  echo -e '\n\n\t\033[4mFarthest insertion heuristic (h1)\033[0m\n'
+elif [[ $h = 2 ]]; then
+  h="h2"
+  echo -e '\n\n\t\033[4mArc insertion heuristic (h2)\033[0m\n'
+else
+  h="h1"
+fi
+
+make clean && make && java tsp/MainTSP -$h $path
